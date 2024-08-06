0365b4db3bdb404a6d6570e211ee84482524d39b
ManthanJadhav1809 <manthan1809@gmail.com>
2024-08-06 19:59:10 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import GameMainPage from './Component/GameMainPage';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="App">
       <h1>Lets Play Rock Paper Scissor Game</h1>
       <GameMainPage></GameMainPage>
     </div>
+    <footer style={{textAlign:"center",marginTop:"25px"}}>
+    <h4>Created By <a style={{textDecoration:"none",cursor:"pointer"}} href='https://manthanjadhav1809.github.io/Manthan_Jadhav_Portfolio/'>Manthan Jadhav</a></h4>
+  </footer>
+    </>
   );
 }
 
